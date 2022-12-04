97c9176acdacc858685e5b5cfb0e173eff74a869
Isaiah Alviola <isaiahalviola0@gmail.com>
2022-12-04 12:13:40 -0500

    nl

---
+++ dbconfig.php
@@ -0,0 +1,11 @@
+<?php
+if(!defined("IN_CODE")){
+    die("***NOT AN ENTRY POINT***");
+}else{
+    $host = "imc.kean.edu";
+    $username = "alviolai";
+    $password = "1147628";
+    $dbname = "CPS3740";
+}
+
+?>
